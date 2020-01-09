e6434490c9fd229e09c7bd66da0de0c4ab303889
Alex Baranowski <alex@euro-linux.com>
2020-01-10 00:03:42 +0100

    els-ASAP auto commit file/tag liveusb-creator-3.11.1-6.0.sl6.src.rpm

---
+++ src/liveusb/releases.py
@@ -0,0 +1,63 @@
+releases = (
+    {'name': 'Fedora 15 i686 Desktop',
+     'sha256': '73203b15e4d43e72378d522a9d3e0098c8c4403a2f1e0e062b70ca1d4b63af74',
+     'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/15/Live/i686/Fedora-15-i686-Live-Desktop.iso'},
+    {'name': 'Fedora 15 i686 KDE',
+     'sha256': '78f509cfa6d71732e36974dc194bce544d38aa1c9f12bc0b897f77d2da3455f5',
+     'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/15/Live/i686/Fedora-15-i686-Live-KDE.iso'},
+    {'name': 'Fedora 15 x86_64 Desktop',
+     'sha256': 'e4007d98d9338419d79fab5d5deb9fcd477117a604d37f89f622141f3d0fa60e',
+     'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/15/Live/x86_64/Fedora-15-x86_64-Live-Desktop.iso'},
+    {'name': 'Fedora 15 x86_64 KDE',
+     'sha256': 'bc8bdb07abc8b8399f7c54dc7ae7a7ee3ea26d262b3ab974700d226d386e3ede',
+     'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/15/Live/x86_64/Fedora-15-x86_64-Live-KDE.iso'},
+    {'name': 'Fedora 14 i686 Desktop',
+     'sha256': '60d05ebdb6882a63f245e609aacb16d47923e1dcb925e9a928f6818a89e17e08',
+     'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/14/Live/i686/Fedora-14-i686-Live-Desktop.iso'},
+     {'name': 'Fedora 14 i686 KDE',
+      'sha256': 'b2419b32c255a63ba122e2c7673bffd80d6293ce085de779aac39816966b45c3',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/14/Live/i686/Fedora-14-i686-Live-KDE.iso'},
+     {'name': 'Fedora 14 x86_64 Desktop',
+      'sha256': '8f81741e938924f68965b5dceac7013d72fe875725b53f61bebd36e740f79b99',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/14/Live/x86_64/Fedora-14-x86_64-Live-Desktop.iso'},
+     {'name': 'Fedora 14 x86_64 KDE',
+      'sha256': 'd0d3890a55e1febf5df5f3624d7f3d57f45a062e9ed21f57119d90c2b2bf27b5',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/14/Live/x86_64/Fedora-14-x86_64-Live-KDE.iso'},
+     {'name': 'Fedora 13 i686',
+      'sha256': '47ccc37db256387b70857f53a6067e8d50e692c9aa85e45e63e5190c5d1e0942',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/13/Live/i686/Fedora-13-i686-Live.iso'},
+     {'name': 'Fedora 13 i686 KDE',
+      'sha256': '6828c02f2bce4ec3bd1ca488df8662640bf53fe7724913310295a5f5f6502cf8',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/13/Live/i686/Fedora-13-i686-Live-KDE.iso'},
+     {'name': 'Fedora 13 x86_64',
+      'sha256': 'c16923e9c2f04af3609c9534f32ea280e4d2c3af50e704344ccb35090f87065a',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/13/Live/x86_64/Fedora-13-x86_64-Live.iso'},
+     {'name': 'Fedora 13 x86_64 KDE',
+      'sha256': '789d7090d39230f0e9b16ed3276974223c120db3120c48e53fa95a6f1604c650',
+      'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/13/Live/x86_64/Fedora-13-x86_64-Live-KDE.iso'},
+
+    ##
+    ## Custom spins
+    ##
+
+    {
+        'name': 'Sugar on a Stick v4 Mango lassi',
+        'url': 'http://alt.fedoraproject.org/pub/alt/spins/linux/releases/14/Spins/i686/Fedora-14-i686-Live-SoaS.iso',
+        'sha256': '32d74ea3dee51ea4643b7ea0143774099533e727f301e7868d399366f26c5471',
+    },
+    {
+        'name': 'Sugar on a Stick v3 Mirabelle',
+        'url': 'http://alt.fedoraproject.org/pub/alt/spins/linux/releases/13/Spins/i686/Fedora-13-i686-Live-SoaS.iso',
+        'sha1': 'b610e7eb6f8128295b643cdfc32101731d8d3f22',
+    },
+    {
+        'name': 'Sugar on a Stick v2 Blueberry',
+        'url': 'http://download.sugarlabs.org/soas/releases/soas-2-blueberry.iso',
+        'sha1': '8022b68433295f12fbfffa66bf49205851a6a1e0',
+    },
+    {
+        'name': 'Sugar on a Stick v1 Strawberry',
+        'url': 'http://download.sugarlabs.org/soas/releases/soas-1-strawberry.iso',
+        'sha1': '41b08c93a65cc1e38286b9f8980f51528a36761d',
+    },
+)
